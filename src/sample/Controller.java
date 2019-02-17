98325df41c3512da2a3aa1980355eb5bb8d588df
mitchellryals <mitchellryals@gmail.com>
2019-02-16 20:32:53 -0500

    syncing builds

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    @FXML
+    private GridPane humanArea;
+
+    @FXML
+    private GridPane botArea;
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,49 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //import the fxml file
+        Pane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Poker");
+
         primaryStage.setScene(new Scene(root, 1200, 900));
         //applies the css file "poker.css" to the project
         root.getStylesheets().add(this.getClass().getResource("poker.css").toExternalForm());
 
+        GridPane botArea = new GridPane();
+        botArea.setAlignment(Pos.TOP_CENTER);
+        botArea.setHgap(10);
+        root.getChildren().add(botArea);
+
+        /*GridPane humanArea = new GridPane();
+        humanArea.setId("humanArea");
+        humanArea.setAlignment(Pos.BOTTOM_CENTER);
+        humanArea.setHgap(10);
+        root.getChildren().add(humanArea);
+        */
+
+        GridPane cardArea = new GridPane();
+        cardArea.setAlignment(Pos.CENTER);
+        cardArea.setHgap(10);
+        root.getChildren().add(cardArea);
+
+        Label l = new Label("hello");
+        //Controller.humanArea.add(l, 1, 1);
+
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
